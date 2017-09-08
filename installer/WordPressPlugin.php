302644e9eeca2aabc50c463589a8450534aa650d
Jonathan Knight <jknight@simplyzesty.com>
2017-09-08 22:23:54 +0100

    Trying to setup a WordPress plugin installer

---
+++ installer/WordPressPlugin.php
@@ -0,0 +1,12 @@
+<?php
+namespace Aloo\WordPress\Installer;
+
+use Composer\Installer\PackageEvent;
+
+class WordPressPlugin {
+    public static function install(PackageEvent $event)
+    {
+        $installedPackage = $event->getOperation()->getPackage();
+        print_r($installedPackage);
+    }
+}
